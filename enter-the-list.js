8c5b875147204fd21cfad50daa8f8905c89b927e
tuyengabi <tuyen.gabi@gmail.com>
2019-04-04 21:38:40 +0200

    enter the list

---
+++ enter-the-list.js
@@ -0,0 +1,18 @@
+     "use strict";
+function getInput() {
+
+}
+	let input = document.getElementById('my-Input').value;
+	console.log(input);
+
+   let btn = document.getElementsByClassName('addBtn')[0]; 
+	console.log(btn);
+	let newLi = document.createElement('li');
+	newLi.innerHTML = input;
+	list.appendChild(newLi);
+	console.log(list.appendChild(newLi));
+	
+  
+function addInputToList() {
+
+} 
