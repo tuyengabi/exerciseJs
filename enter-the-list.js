b8ee15b6d5a197aee18e7f35ea40bc2f64443880
tuyengabi <tuyen.gabi@gmail.com>
2019-04-08 00:09:22 +0200

    24h

--- enter-the-list.js
+++ enter-the-list.js
@@ -3,8 +3,11 @@
 
 function addValueToList() {
 	let myul = document.getElementsByTagName('ul')[0];
+	console.log(myul);
 	let newLi= document.createElement('li');
+	console.log(newLi);
 	let valueinput = document.getElementById('my-Input').value;
+	console.log(valueinput);
 
 	if (valueinput == '') {
 		return;
